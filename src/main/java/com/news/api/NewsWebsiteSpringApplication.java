0060c5d988516da7fae85974de6dabd030496963
lucas cruz <lucas_cruz100@live.com>
2023-01-31 10:41:44 -0300

    first commit

---
+++ src/main/java/com/news/api/NewsWebsiteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.news.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebsiteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWebsiteSpringApplication.class, args);
+	}
+
+}
